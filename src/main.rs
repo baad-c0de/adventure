dc3ec5102896cf41affa302dadcd514b5cc457de
Baad C0de <baadc0de@proton.me>
2023-04-13 20:26:48 +0100

    Extract the verb from the input.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use std::io::{stdin, stdout, Write};
 
+/// A verb is either unknown or known.
+///
+/// Represents the first word in a command.
+#[derive(Debug, PartialEq, Eq)]
+enum Verb {
+    /// The verb is unknown.
+    Unknown,
+
+    /// The verb is known and the data is the index of the verb in the
+    /// verbs array.
+    Known(u32),
+}
+
 fn main() {
     let stdin = stdin();
     let mut stdout = stdout();
+
+    let verbs = vec!["quit"];
 
     loop {
         print!("> ");
@@ -10,6 +25,29 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        println!("{}", input);
+        let words = input
+            .split_whitespace()
+            .map(|s| s.to_ascii_lowercase())
+            .collect::<Vec<_>>();
+
+        // Find the verb.
+        let verb = if words.len() == 0 {
+            Verb::Unknown
+        } else {
+            let verb = &words[0];
+            if let Some(index) = verbs.iter().position(|v| v == verb) {
+                Verb::Known(index as u32)
+            } else {
+                Verb::Unknown
+            }
+        };
+
+        println!("{:?}", words);
+        println!("{:?}", verb);
+
+        // Quit the game.
+        if verb == Verb::Known(0) {
+            break;
+        }
     }
 }
